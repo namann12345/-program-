736f24f03a46a83fff746a742e0e515ad78c3b2a
Naman Gupta <142799953+namann12345@users.noreply.github.com>
2023-12-02 20:53:43 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,21 +1,21 @@
-# creating a tuple using ()
+ creating a tuple using ()
 
-#t = (1,2,4,5)
+t = (1,2,4,5)
 
-# t1 = () # Empty tuple  
-#t1 = (1) # wrong way to declare a tuple with single element 
-#t1 = (1,) # tuple with single element
-#print(t1)
+ t1 = () # Empty tuple  
+t1 = (1) # wrong way to declare a tuple with single element 
+t1 = (1,) # tuple with single element
+print(t1)
 
   
-# printing the elements of a tuple
+ printing the elements of a tuple
 
-#print(t[0])
+print(t[0])
 
 
-# cannot update the values of a tuple
+ cannot update the values of a tuple
 
-#t[0] = 34
+t[0] = 34
 
 t = (1,2,4,5,4,1,2,1,1)
 print(t.count(1))
