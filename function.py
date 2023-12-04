499f484f300d4336527c3752cffc169f18cbc50a
Naman Gupta <142799953+namann12345@users.noreply.github.com>
2023-12-04 23:22:37 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,21 +1,21 @@
-#def greet(name):
- #   print("Good Day, "+ name)
+def greet(name):
+    print("Good Day, "+ name)
 
-#def mySum(num1,num2):
- #   return num1+num2 
-#greet("naman")
-#s = mySum(6,32)
-#print(s)
+def mySum(num1,num2):
+    return num1+num2 
+greet("naman")
+s = mySum(6,32)
+print(s)
 
 
 
-#n! = 1*2*3*4..*n
-#n=0
-#n=4
-#for i in range(n):
- #   product = product*(i+1)
+n! = 1*2*3*4..*n
+n=0
+n=4
+for i in range(n):
+    product = product*(i+1)
  
-# print(product)
+ print(product)
 
 def factorial_iter(n):
     product = 1
